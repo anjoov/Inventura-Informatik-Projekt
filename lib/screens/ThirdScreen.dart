34ab7ac1fe3c6a35ba359e804302be22e636c1b5
Jesper Nilsson <jespernilsson@gmx.de>
2023-03-15 12:30:17 +0100

    qr scanner/generator

--- lib/screens/ThirdScreen.dart
+++ lib/screens/ThirdScreen.dart
@@ -1,7 +1,13 @@
 // ignore_for_file: prefer_const_constructors
 
 import "package:flutter/material.dart";
+import 'package:flutter/services.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:barcode_widget/barcode_widget.dart';
+
+// bar code scanner
 
 class ThirdScreen extends StatefulWidget {
   const ThirdScreen({Key? key}) : super(key: key);
@@ -11,17 +17,71 @@ class ThirdScreen extends StatefulWidget {
 }
 
 class ThirdScreenState extends State<ThirdScreen> {
+  String? scanResult;
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home:Scaffold(
-        body: Container(
-          color: Colors.purple,
-        )
+  Widget build(BuildContext context) => Scaffold(
+      appBar: AppBar(
+        title: Text("Scan Barcode"),
+        centerTitle: true,
+      ),
+      body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+            ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.amber,
+                onPrimary: Colors.black,
+              ),
+              icon: Icon(Icons.camera_alt_outlined),
+              label: Text("start scan"),
+              onPressed: scanBarcode,
+            ),
+            SizedBox(height: 20),
+            Text(
+              scanResult == null ? "Scan a code!" : "Scan result: $scanResult",
+              style: TextStyle(fontSize: 18),
+            )
+          ])));
 
+  Future scanBarcode() async {
+    String scanResult;
+    try {
+      scanResult = await FlutterBarcodeScanner.scanBarcode(
+        "#ff6666",
+        "Cancel",
+        true,
+        ScanMode.BARCODE,
+      );
+    } on PlatformException {
+      scanResult = "Failed to get platform version.";
+    }
+    if (!mounted) return;
 
-      )
-    );
+    setState(() => this.scanResult = scanResult);
   }
 }
 
+/*
+appBar: AppBar(
+            title: Text("Barcode Scanner/Generator")
+            centerTitle: true,
+          ),
+
+Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    BarcodeWidget(data: data, barcode: barcode)
+                    SizedBox(height: 24),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 32),
+                      child: buildTextField(context)
+                    ),
+                  ],
+                ),
+            ),
+          )
+*/
